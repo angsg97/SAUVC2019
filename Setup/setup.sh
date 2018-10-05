7e2db8d413c46786d480acc8728b45352ffb375e
heyuhang0 <heyuhang0@outlook.com>
2018-10-05 23:48:52 +0800

    add setup script and environment test code

---
+++ Setup/setup.sh
@@ -0,0 +1,49 @@
+read -p "Press [enter] to continue"
+
+# Download the Miniconda install script and verify it
+
+wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-`uname -m`.sh
+md5sum Miniconda3-latest-Linux-`uname -m`.sh
+
+# Install Miniconda! Then delete the install file
+
+bash Miniconda3-latest-Linux-`uname -m`.sh
+rm Miniconda3-latest-Linux-`uname -m`.sh
+
+source ~/.bashrc
+export PATH="$HOME/miniconda3/bin:$PATH"
+
+# setup conda in bashrc
+declare -a file_appends=(
+	'. $HOME/miniconda3/etc/profile.d/conda.sh'
+)
+
+target_file="$HOME/.bashrc"
+
+for i in "${file_appends[@]}" ; do
+  if ! grep -Fxq "$i" "$target_file" ; then
+	echo "$i" >> "$target_file"
+  else
+	:
+  fi
+done
+
+# create conda environment
+echo -e "\nCreating SAUVC Conda Environment..."
+conda create -n SAUVC python=3.6
+
+# Activate the SAUVC conda environment
+echo -e "\nActivating conda SAUVC environment..." 
+source activate SAUVC
+
+# Install dependencies
+echo -e "\nInstalling dependencies..."
+pip install imutils
+conda install numpy scipy matplotlib cython
+conda install -c menpo opencv3
+
+# run test code
+python test.py
+
+# Done
+source ~/.bashrc
