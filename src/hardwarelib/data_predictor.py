15a62ad4396b1785f3285e81d7e80447d4baf1ca
luo <lsjsutdsg@gmail.com>
2018-10-12 22:49:55 +0800

    fixed bug of cannot work when enter the same value as the last time

--- src/hardwarelib/data_predictor.py
+++ src/hardwarelib/data_predictor.py
@@ -2,27 +2,34 @@ from collections import deque
 
 
 class ParabolaPredictor():
-    def __init__(self):
-        self.past_value = deque(maxlen=3)
+    # please do not set a value that is more than 3
+    def __init__(self,n=3): 
+        self.past_value = deque(maxlen=n)
 
     def put_data(self, time, data):
         piont = (time, data)
+        for t,v in self.past_value:
+            if time==t:
+                return
+            else:
+                pass
         self.past_value.append(piont)
+        print(self.past_value)
 
     def predict(self, time):
         if len(self.past_value) == 1:
-            return self.past_value[0][1]
+            return self.past_value[-1][1]
         elif len(self.past_value) == 2:
-            k = (self.past_value[0][1]-self.past_value[1][1])\
-                / (self.past_value[0][0]-self.past_value[1][0])
-            return k*(time-self.past_value[1][0]) + self.past_value[1][1]
+            k = (self.past_value[-2][1]-self.past_value[-1][1])\
+                / (self.past_value[-2][0]-self.past_value[-1][0])
+            return k*(time-self.past_value[-1][0]) + self.past_value[-1][1]
         else:
-            x1 = self.past_value[0][0]
-            y1 = self.past_value[0][1]
-            x2 = self.past_value[1][0]
-            y2 = self.past_value[1][1]
-            x3 = self.past_value[2][0]
-            y3 = self.past_value[2][1]
+            x1 = self.past_value[-3][0]
+            y1 = self.past_value[-3][1]
+            x2 = self.past_value[-2][0]
+            y2 = self.past_value[-2][1]
+            x3 = self.past_value[-1][0]
+            y3 = self.past_value[-1][1]
             a = -(-x2*y1+x3*y1+x1*y2-x3*y2-x1*y3+x2*y3) / \
                 ((-x1+x2)*(x2-x3)*(-x1+x3))
             b = -(x2*x2*y1-x3*x3*y1-x1*x1*y2+x3*x3*y2+x1 *
@@ -54,3 +61,10 @@ if __name__ == "__main__":
         print(True)
     else:
         print("error")
+
+    a.put_data(4,90)
+    z=a.predict(5)
+    print(z)
+    a.put_data(4,90)
+    zx=a.predict(6)
+
