4d4d7842c7e15cc65d3d31552be4088ad499217f
heyuhang0 <heyuhang0@outlook.com>
2018-10-15 01:33:32 +0800

    Improve steering proformance while going forward, safer close method

--- src/cvcar/mcu.py
+++ src/cvcar/mcu.py
@@ -1,4 +1,5 @@
 import serial
+import time
 
 
 class MCU():
@@ -9,11 +10,14 @@ class MCU():
         self.upper_limit = upper_limit
         self.lower_limit = lower_limit
         self.deadzone = deadzone
+        self.stopped = False
         if not self.ser.isOpen():
             self.ser.open()
 
     def stop(self):
         self.set_motors(0, 0)
+        self.stopped = True
+        time.sleep(0.1)
         self.ser.close()
 
     def refresh(self):
@@ -21,8 +25,16 @@ class MCU():
                         str(self.right_power)).encode())
 
     def __check_range(self, power):
-        power_abs = abs(power)
-        power_abs = 1 if power_abs > 1 else power_abs
+        if power < -1:
+            return -1
+        elif power > 1:
+            return 1
+        return power
+    
+    def __linear_mapping(self, power, check_range=True):
+        if self.stopped:
+            return 0
+        power_abs = self.__check_range(abs(power))
         power_sign = 1 if power > 0 else -1
 
         if power_abs < self.deadzone:
@@ -31,14 +43,16 @@ class MCU():
             return power_sign * int(power_abs * (self.upper_limit - self.lower_limit) + self.lower_limit)
 
     def set_left_motor(self, power):
-        self.left_power = self.__check_range(power)
+        self.left_power = self.__linear_mapping(power)
         self.refresh()
 
     def set_right_motor(self, power):
-        self.right_power = self.__check_range(power)
+        self.right_power = self.__linear_mapping(power)
         self.refresh()
 
     def set_motors(self, power_forward, power_turn_L):
-        self.left_power = self.__check_range(power_forward + power_turn_L)
-        self.right_power = self.__check_range(power_forward - power_turn_L)
+        power_forward = self.__check_range(power_forward)
+        power_turn_L = self.__check_range(power_turn_L)
+        self.left_power = self.__linear_mapping(power_forward + power_turn_L)
+        self.right_power = self.__linear_mapping(power_forward - power_turn_L)
         self.refresh()
