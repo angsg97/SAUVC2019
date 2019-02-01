2d6514bcc9f8f353701a9386e59d3fbd8c1997c7
lsjsg <lsjsutdsg@gmail.com>
2019-02-01 15:49:56 +0800

    a worse way to find a gate

--- src/tracking/core_gate.py
+++ src/tracking/core_gate.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 from tracking import ITrackingCore
 
+
 class GateTrackerV2(ITrackingCore):
 
     class TwoPointLine:
@@ -67,19 +68,28 @@ class GateTrackerV2(ITrackingCore):
         vec_D = self.Vector.from_line(line_D)
 
         # check parallel
-        if vec_L.angle(vec_R) > 10:
-            return False
+        virtical_angle_score = vec_L.angle(vec_R)
+        horizontal_angle_score_U = abs(vec_L.angle(vec_U) - 90) / 40
+        horizontal_angle_score_D = abs(vec_L.angle(vec_D) - 90) / 40
 
-        if not (70 < vec_L.angle(vec_U) < 110 and 70 < vec_R.angle(vec_U) < 100):
-            return False
+        def parameter(error):
+            return 1 / (error**8 + 1)
 
-        if not (70 < vec_L.angle(vec_D) < 110 and 70 < vec_R.angle(vec_D) < 100):
-            return False
+        def standard_normal_distribution(error, delta):
+            # delta = 5
+            return (0.3989422804 / delta) * 2.7182818285**(-error*error/(2*delta*delta))
 
-        if not 2/3 < (vec_L.add_vector(vec_R).norm() / vec_D.add_vector(vec_U).norm()) < 1.5:
-            return False
+        # parameters
+        a, b, c = 1000000000, 1000000, 1000
+        ratio_score = abs((vec_L.norm() + vec_R.norm()) /
+                          (vec_U.norm() + vec_D.norm()) - 1)
+        num1, num2, num3, num4 = a * standard_normal_distribution(ratio_score, 2), b * standard_normal_distribution(
+            virtical_angle_score, 1), c * standard_normal_distribution(horizontal_angle_score_D, 1), c * standard_normal_distribution(horizontal_angle_score_U, 1)
 
-        return True
+        print("ratio_score:%f\t virtical_score:%f\t horisontalD:%f\t horisontalU:%f\t" % (
+            num1, num2, num3, num4))
+        return num1 + num2 + num3 + num4
+        # return a * parameter(ratio_score) + b * parameter(virtical_angle_score) + c * parameter(horizontal_angle_score_D) + c * parameter(horizontal_angle_score_U)
 
     def find(self, frame):
         # Splite channelsec
@@ -110,23 +120,44 @@ class GateTrackerV2(ITrackingCore):
                     line2 = self.TwoPointLine(line2[0])
                     if not self.__possible_line(line2):
                         continue
-                    if self.__possible_gate(line1, line2):
-                        gates.append((line1, line2))
+                    gates.append(
+                        (self.__possible_gate(line1, line2), line1, line2))
 
-        def gate_total(gate):
-            line1, line2 = gate
-            return (abs(line1.y1 - line1.y2) +
-                    abs(line2.y1 - line2.y2))
+        def score(gate):
+            return gate[0]
         x = None
         width = None
+        gates.sort(key=score, reverse=False)
         if gates:
-            gates.sort(key=gate_total, reverse=True)
             gate = gates[0]
-            line1, line2 = gate
-            x = (line1.x1 + line1.x2 + line2.x1 + line2.x2) / 4 - (frame.shape[1] // 2)
+            score, line1, line2 = gate
+            x = (line1.x1 + line1.x2 + line2.x1 + line2.x2) / \
+                4 - (frame.shape[1] // 2)
             width = abs((line1.x1 + line1.x2) - (line2.x1 + line2.x2)) / 2
             cv2.line(results_frame, (line1.x1, line1.y1),
                      (line1.x2, line1.y2), (0, 0, 255), 3)
             cv2.line(results_frame, (line2.x1, line2.y1),
                      (line2.x2, line2.y2), (0, 0, 255), 3)
         return (x, None, width, [blurred, edges, lines_frame, results_frame])
+
+        # if self.__possible_gate(line1, line2):
+        #     gates.append((line1, line2))
+
+        # def gate_total(gate):
+        #     line1, line2 = gate
+        #     return (abs(line1.y1 - line1.y2) +
+        #             abs(line2.y1 - line2.y2))
+        # x = None
+        # width = None
+        # if gates:
+        #     # gates.sort(key=gate_total, reverse=True)
+        #     gates.sort()
+        #     gate = gates[0]
+        #     line1, line2 = gate
+        #     x = (line1.x1 + line1.x2 + line2.x1 + line2.x2) / 4 - (frame.shape[1] // 2)
+        #     width = abs((line1.x1 + line1.x2) - (line2.x1 + line2.x2)) / 2
+        #     cv2.line(results_frame, (line1.x1, line1.y1),
+        #              (line1.x2, line1.y2), (0, 0, 255), 3)
+        #     cv2.line(results_frame, (line2.x1, line2.y1),
+        #              (line2.x2, line2.y2), (0, 0, 255), 3)
+        # return (x, None, width, [blurred, edges, lines_frame, results_frame])
